353726608e21fc6afe2d5dafbcdac613e446eb95
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-01-08 18:34:45 +0530

    Adding new go file for first client go

---
+++ kubernetes/client-go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := flag.String("kubeconfig", "~/.kube/config", "location to your kubeconfig file")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+
+	if err != nil {
+		// handle error
+	}
+	fmt.Println(config)
+}
