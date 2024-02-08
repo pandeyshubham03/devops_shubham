521990ffa5a799a306838cdd73e10f6423553e0a
pandeyshubham03 <shubham.p20062003@gmail.com>
2024-02-08 20:06:31 +0530

    HTTP Get from API

--- Go/go-helloworld/main.go
+++ Go/go-helloworld/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -10,9 +14,27 @@ func main() {
 	args := os.Args
 
 	if len(args) < 1 {
-		fmt.Printf("Usage: ./hello-world <argument>\n")
+		fmt.Printf("Usage: ./http-get <url>\n")
 		os.Exit(1)
 	}
 
-	fmt.Printf("Hello World!\nArguments: %v\n", args[1]) // f stands for format
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("Url is in invalid format: %s\n", err)
+	}
+
+	response, err := http.Get(args[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
 }
